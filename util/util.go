84279cec5258b6c4bd8340c5e83ba8af86ebfc38
donnie4w <donnie4w@gmail.com>
2024-07-20 16:12:35 +0800

    Update util.go

--- util/util.go
+++ util/util.go
@@ -7,8 +7,51 @@
 
 package util
 
-import "strings"
+import (
+	"log"
+	"os"
+	"strings"
+)
 
 func ToUpperFirstLetter(s string) string {
 	return strings.ToUpper(string(s[0])) + s[1:]
+}
+
+func CreeteInitfile(filename string) {
+	if isFileExist(filename) {
+		log.Println(filename, " is exist")
+		return
+	}
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	_, err = file.Write([]byte(jsonData))
+	if err != nil {
+		panic(err)
+	}
+	log.Println(filename, "  is created successfully")
+}
+
+var jsonData = `{
+  "dbtype": "mysql",
+  "dbhost": "localhost",
+  "dbport": 3306,
+  "dbname": "hstest",
+  "dbuser": "root",
+  "dbpwd": "123456",
+  "package": "dao",
+  "table": [""],
+  "table_alias": [{"table": "","alias": ""}],
+  "table_except": [""]
+}`
+
+func isFileExist(path string) (_r bool) {
+	if path != "" {
+		_, err := os.Stat(path)
+		_r = err == nil || os.IsExist(err)
+	}
+	return
 }
