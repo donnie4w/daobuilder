bc63cc56395fe555c9d653e4d33c175708b34fc8
donnie4w <donnie4w@gmail.com>
2024-07-25 11:54:14 +0800

    Update main.go

--- gdao/main.go
+++ gdao/main.go
@@ -10,7 +10,7 @@ var gdaoversion = "1.1.0"
 
 func main() {
 	if len(os.Args) == 2 && os.Args[1] == "init" {
-		util.CreeteInitfile("mysql.json")
+		util.CreeteInitfile("gdao.json")
 		return
 	}
 	log.Println(os.Args)
